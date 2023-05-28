1fd13b7760f7a176bf00490a6254c0520e3f36b2
hpekkan <hpekkan@etu.edu.tr>
2023-05-28 16:09:05 +0300

    added matches button

--- src/components/components/Header/index.jsx
+++ src/components/components/Header/index.jsx
@@ -3,7 +3,10 @@ import {logout} from "../../../services/firebase";
 import { useState } from "react";
 const Header = ({ PageInd, setPageInd}) => {
   const groups = () => {
-    setPageInd(PageInd===0?1:0);
+    setPageInd(1);
+  };
+  const matches = () => {
+    setPageInd(0);
   };
 
   return (
@@ -13,6 +16,8 @@ const Header = ({ PageInd, setPageInd}) => {
         alt="TOBB ETU"
       />
       <h2>TOBB World Cup Scoreboard</h2>
+      <button className="logout" onClick={matches}
+       >Matches</button>
       <button className="logout" onClick={groups}
        >Groups</button>
       <button className="groups" onClick={logout}
