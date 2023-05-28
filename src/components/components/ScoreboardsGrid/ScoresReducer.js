9fa9c268ed197b6082ae59a7173b060804907c64
hpekkan <hpekkan@etu.edu.tr>
2023-05-28 14:45:28 +0300

    fetch volleybal data

--- src/components/components/ScoreboardsGrid/ScoresReducer.js
+++ src/components/components/ScoreboardsGrid/ScoresReducer.js
@@ -1,148 +1,150 @@
+import SheetsService from "../../../services/sheets.service";
+
 const teamsMap = {
-    1: 'homeTeam',
-    2: 'awayTeam'
-}
+  1: "homeTeam",
+  2: "awayTeam",
+};
 
 const sortGamesByTotalScore = (currentGame, nextGame) => {
-    const currentGameTotalScore = currentGame.homeTeam.score + currentGame.awayTeam.score;
-    const nextGameTotalScore = nextGame.homeTeam.score + nextGame.awayTeam.score;
-    return nextGameTotalScore - currentGameTotalScore;
-}
+  const currentGameTotalScore =
+    currentGame.homeTeam.score + currentGame.awayTeam.score;
+  const nextGameTotalScore = nextGame.homeTeam.score + nextGame.awayTeam.score;
+  return nextGameTotalScore - currentGameTotalScore;
+};
+
 export const actionTypes = {
-    START_GAME: 'start',
-    UPDATE_SCORE: 'update',
-    FINISH_GAME: 'finish'
-}
+  START_GAME: "start",
+  UPDATE_SCORE: "update",
+  FINISH_GAME: "finish",
+  FETCH_GAMES: "fetch", // Add FETCH_GAMES action type
+};
+
 export const initialState = {
-    finishedGames: [],
-    games: [
-        {
-            gameId: 0,
-            startedGame: false,
-            homeTeam: {
-                name: 'Mexico',
-                countryCode: 'mx',
-                score: 0
-            },
-            awayTeam: {
-                name: 'Canada',
-                countryCode: 'ca',
-                score: 0
-            }
-        },
-        {
-            gameId: 1,
-            startedGame: false,
-            homeTeam: {
-                name: 'Spain',
-                countryCode: 'es',
-                score: 0
-            },
-            awayTeam: {
-                name: 'Brazil',
-                countryCode: 'br',
-                score: 0
-            }
-        },
-        {
-            gameId: 2,
-            startedGame: false,
-            homeTeam: {
-                name: 'Germany',
-                countryCode: 'de',
-                score: 0
-            },
-            awayTeam: {
-                name: 'France',
-                countryCode: 'fr',
-                score: 0
-            }
-        },
-        {
-            gameId: 3,
-            startedGame: false,
-            homeTeam: {
-                name: 'Uruguay',
-                countryCode: 'uy',
-                score: 0
-            },
-            awayTeam: {
-                name: 'Italy',
-                countryCode: 'it',
-                score: 0
-            }
-        },
-        {
-            gameId: 4,
-            startedGame: false,
-            homeTeam: {
-                name: 'Argentina',
-                countryCode: 'ar',
-                score: 0
-            },
-            awayTeam: {
-                name: 'Australia',
-                countryCode: 'au',
-                score: 0
-            }
-        },
-    ]
+  finishedGames: [],
+  games: [],
 };
 
 const reducer = (state, action) => {
-    const data = action.data;
-    const { gameId } = data;
+  const data = action.data;
+  const { gameId } = data;
 
-    switch (action.type) {
-        case actionTypes.START_GAME:
-            console.log(">>> START_GAME: gameId:", gameId);
-            return {
-                ...state,
-                games: state.games.map((game) => game.gameId === gameId ? { ...game, startedGame: true } : game)
-            }
-        case actionTypes.UPDATE_SCORE:
-            const { teamId } = data;
+  switch (action.type) {
+    case actionTypes.START_GAME:
+      console.log(">>> START_GAME: gameId:", gameId);
+      return {
+        ...state,
+        games: state.games.map((game) =>
+          game.gameId === gameId ? { ...game, startedGame: true } : game
+        ),
+      };
+    case actionTypes.UPDATE_SCORE:
+      const { teamId } = data;
 
-            // Don't update the score if the game has not started yer
-            const isGameStarted = state.games.find(game => game.gameId === gameId && game.startedGame === true);
-            if (!isGameStarted) {
-                return state;
-            }
+      // Don't update the score if the game has not started yet
+      const isGameStarted = state.games.find(
+        (game) => game.gameId === gameId && game.startedGame === true
+      );
+      if (!isGameStarted) {
+        return state;
+      }
 
-            console.log(">>> UPDATE_SCORE: gameId:", gameId, " | team: ", teamsMap[teamId]);
+      console.log(
+        ">>> UPDATE_SCORE: gameId:",
+        gameId,
+        " | team: ",
+        teamsMap[teamId]
+      );
 
-            // Increment the goals value of the team who scored
-            const team = teamsMap[teamId];
-            return {
-                ...state,
-                games: state.games.map((game) => game.gameId === gameId ?
-                    {
-                        ...game,
-                        [team]: {
-                            ...game[team],
-                            score: game[team].score + 1
-                        }
-                    } :
-                    game)
-            }
-        case actionTypes.FINISH_GAME:
-            console.log(">>> FINISH_GAME: gameId:", gameId);
+      // Increment the goals value of the team who scored
+      const team = teamsMap[teamId];
+      return {
+        ...state,
+        games: state.games.map((game) =>
+          game.gameId === gameId
+            ? {
+                ...game,
+                [team]: {
+                  ...game[team],
+                  score: game[team].score + 1,
+                },
+              }
+            : game
+        ),
+      };
+    case actionTypes.FINISH_GAME:
+      console.log(">>> FINISH_GAME: gameId:", gameId);
 
-            return {
-                ...state,
-                games: state.games.filter(game => game.gameId !== gameId),
-                finishedGames: [
-                    ...state.finishedGames,
-                    state.games.find((game) => game.gameId === gameId)
-                ].filter(Boolean) // filter(Boolean) keeps the array clean, i.e. removes `undefined` or `null`
-                 .sort(sortGamesByTotalScore)
-                 .map(game => {
-                     game.startedGame = false;
-                     return game;
-                 })
-            }
-        default:
-            throw new Error('Unrecognized action type. Please check ScoresReducer.');
-    }
-}
+      return {
+        ...state,
+        games: state.games.filter((game) => game.gameId !== gameId),
+        finishedGames: [
+          ...state.finishedGames,
+          state.games.find((game) => game.gameId === gameId),
+        ]
+          .filter(Boolean) // filter(Boolean) keeps the array clean, i.e. removes `undefined` or `null`
+          .sort(sortGamesByTotalScore)
+          .map((game) => {
+            game.startedGame = false;
+            return game;
+          }),
+      };
+    case actionTypes.FETCH_GAMES: // Handle FETCH_GAMES action type
+      return {
+        ...state,
+        games: data.games,
+      };
+    default:
+      throw new Error("Unrecognized action type. Please check ScoresReducer.");
+  }
+};
+
+export const fetchMatches = async () => {
+  try {
+    const response = await SheetsService.fetchMatches();
+    const matches = response.values;
+    const games = matches.map((match, index) => {
+      if (match.length < 7) {
+        return {
+          gameId: match[0], // Event ID
+          startedGame: false,
+          eventDate: match[4], // Event Date
+          homeTeam: {
+            name: match[2], // Event Name
+            countryCode: "ca",
+            score:0, // Parse the home score as an integer
+          },
+          awayTeam: {
+            name: match[3], // Event Description
+            countryCode: "mx",
+            score: 0, // Parse the away score as an integer
+          },
+        };
+      }
+      const score = match[6]; // Event Result
+      const [homeScore, awayScore] = score.split("-");
+      return {
+        gameId: match[0], // Event ID
+        startedGame: false,
+        eventDate: match[1], // Event Date
+        homeTeam: {
+          name: match[2], // Event Name
+          countryCode: "ca",
+          score: parseInt(homeScore), // Parse the home score as an integer
+        },
+        awayTeam: {
+          name: match[3], // Event Description
+          countryCode: "mx",
+          score: parseInt(awayScore), // Parse the away score as an integer
+        },
+      };
+    });
+    return {
+      games,
+    };
+  } catch (error) {
+    console.error("Error fetching matches:", error);
+    return initialState;
+  }
+};
+
 export default reducer;
--- src/services/sheets.service.js
+++ src/services/sheets.service.js
@@ -1,20 +1,41 @@
-// A Grubu: https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!B2:B7?key=AIzaSyBcU87uWgq9hmvVeo8jrGl12AwzetK8NOU
-// B Grubu: https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!B9:B14?key=AIzaSyBcU87uWgq9hmvVeo8jrGl12AwzetK8NOU
-// C Grubu: https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!B16:B21?key=AIzaSyBcU87uWgq9hmvVeo8jrGl12AwzetK8NOU
-// D Grubu: https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!B23:B28?key=AIzaSyBcU87uWgq9hmvVeo8jrGl12AwzetK8NOU
-// Maç 1 : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!D3:J3?key=AIzaSyBcU87uWgq9hmvVeo8jrGl12AwzetK8NOU
-// 40 maç var
-// Çeyrek 1 -41. maç  : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!M7:M10?key=AIzaSyBcU87uWgq9hmvVeo8jrGl12AwzetK8NOU
-// Çeyrek 2 -42. maç  : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!M12:M15?key=AIzaSyBcU87uWgq9hmvVeo8jrGl12AwzetK8NOU
-// Çeyrek 3 -43. maç  : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!M17:M20?key=AIzaSyBcU87uWgq9hmvVeo8jrGl12AwzetK8NOU
-// Çeyrek 4 -44. maç  : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!M22:M25?key=AIzaSyBcU87uWgq9hmvVeo8jrGl12AwzetK8NOU
-// Yarı 1 -45. maç  : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!O10:O13?key=AIzaSyBcU87uWgq9hmvVeo8jrGl12AwzetK8NOU
-// Yarı 2 -46. maç  : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!O20:O23?key=AIzaSyBcU87uWgq9hmvVeo8jrGl12AwzetK8NOU
-// 3.lük -47. maç  : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!Q15:Q18?key=AIzaSyBcU87uWgq9hmvVeo8jrGl12AwzetK8NOU
-// Final -48. maç  : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!R15:R18?key=AIzaSyBcU87uWgq9hmvVeo8jrGl12AwzetK8NOU
-//Puanlar
-// A Grubu  : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!T7:Y12?key=AIzaSyBcU87uWgq9hmvVeo8jrGl12AwzetK8NOU
-// B Grubu  : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!T14:Y19?key=AIzaSyBcU87uWgq9hmvVeo8jrGl12AwzetK8NOU
-// C Grubu  : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!T21:Y26?key=AIzaSyBcU87uWgq9hmvVeo8jrGl12AwzetK8NOU
-// D Grubu  : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!T28:Y33?key=AIzaSyBcU87uWgq9hmvVeo8jrGl12AwzetK8NOU
+// A Group: https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!B2:B7?key={apiKey}
+// B Group: https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!B9:B14?key={apiKey}
+// C Group: https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!B16:B21?key={apiKey}
+// D Group: https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!B23:B28?key={apiKey}
+// Match 1-40 : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!D3:J42?key={apiKey}
+// Q1 -41st Match  : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!M7:M10?key={apiKey}
+// Q2 -42nd Match  : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!M12:M15?key={apiKey}
+// Q3 -43th Match  : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!M17:M20?key={apiKey}
+// Q4 -44th Match  : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!M22:M25?key={apiKey}
+// S1 -45th Match  : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!O10:O13?key={apiKey}
+// S2 -46th Match  : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!O20:O23?key={apiKey}
+// third place match match -47th Match  : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!Q15:Q18?key={apiKey}
+// Final -48th Match  : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!R15:R18?key={apiKey}
+//Scores
+// A Group  : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!T7:Y12?key={apiKey}
+// B Group  : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!T14:Y19?key={apiKey}
+// C Group  : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!T21:Y26?key={apiKey}
+// D Group  : https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!T28:Y33?key={apiKey}
 // TODO: ADD functions for api calls
+
+import axios from 'axios';
+
+const baseUrl =
+  "https://sheets.googleapis.com/v4/spreadsheets/15vUn5-byqvgrjmQiU8ciLDVpaePYNDH6jSj1_sfbbgw/values/Fikst%C3%BCr!";
+const apiKey = process.env.REACT_APP_GOOGLE_SHEETS_API_KEY;
+
+async function fetchMatches() {
+    try {
+      const response = await axios.get(`${baseUrl}D3:J42?key=${apiKey}`);
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching matches:", error);
+      throw error;
+    }
+  }
+
+const SheetsService = {
+  fetchMatches,
+};
+
+export default SheetsService;
